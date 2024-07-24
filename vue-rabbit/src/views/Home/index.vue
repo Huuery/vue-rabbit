62b7634da0bf7aae73d3ba2a190c530b1158b8ce
Huuj <huu@outlook.cl>
2024-07-24 16:18:10 +0800

    插槽

--- vue-rabbit/src/views/Home/index.vue
+++ vue-rabbit/src/views/Home/index.vue
@@ -5,6 +5,8 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeCategory from "./components/HomeCategory.vue";
 import HomeProduct from "./components/HomeProduct.vue";
 
+import HomePanel from "./components/HomePanel.vue";
+
 import { useBannerStore } from "@/stores/banner";
 import { onMounted } from "vue";
 const bannerStore = useBannerStore();
@@ -19,4 +21,14 @@ onMounted(() => bannerStore.getBanner())
     <HomeNew></HomeNew>
     <HomeHot></HomeHot>
     <HomeProduct></HomeProduct>
+    <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
+        <div>
+            我是新鲜好物
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
+        <div>
+            我是人气推荐
+        </div>
+    </HomePanel>
 </template>
